d2e5c714b41fb7865a327887fe6efabbea9eaea6
Austin Dulaney <adulaney@caltech.edu>
2016-09-14 11:46:10 -0700

    Added hacker_stats.py and add bootstrap function to bootcamp_utils.py

--- bootcamp_utils.py
+++ bootcamp_utils.py
@@ -12,3 +12,17 @@ def ecdf(data):
     x = np.sort(data)
     y = np.arange(1, 1+len(x)) / len(x)
     return x, y
+
+
+def draw_bs_reps(data, func, size=1, interval=[2.5, 97.5]):
+    """
+    Generates bootstrap replicas of data for desired statistic.
+    """
+    n = len(data)
+    bs_replicates = np.empty(size)
+    # Generate bootstrap replicas
+    for i in range(size):
+        bs_sample = np.random.choice(data, replace=True, size=n)
+        bs_replicates[i] = func(bs_sample)
+    conf_int = np.percentile(bs_replicates_1975, interval)
+    return bs_replicates conf_int
