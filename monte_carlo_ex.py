a88a35cf05590ee6ab6505c3f81f01380e53e2cc
Austin Dulaney <adulaney@caltech.edu>
2016-09-15 13:00:07 -0700

    Added test folder, tdd files, and monte carlo

---
+++ monte_carlo_ex.py
@@ -0,0 +1,12 @@
+""" Exercise 4.3 """
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import bootcamp_utils as bcut
+import numba
+
+@numba.jit(nopython=True)
+def backtrack_steps(tau=0.5)
+    steps = 0
